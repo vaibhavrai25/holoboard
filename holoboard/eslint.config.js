4f96b14ffcde44e63db9d6b9cd6e0bc41781f2b4
PURUSHOTTAM06 <purushottam.2006.ms@gmail.com>
2025-12-07 01:02:46 +0530

    Fix ESLint config extends syntax

--- holoboard/eslint.config.js
+++ holoboard/eslint.config.js
@@ -8,7 +8,7 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    extends: [bbb
+    extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
